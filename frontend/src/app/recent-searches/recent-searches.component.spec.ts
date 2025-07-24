cdf7dcd02e4c7ae8b9b2c1bf86b91211bc970ef0
Aaron Shim <5382864+aaronshim@users.noreply.github.com>
2025-07-24 21:00:52 +0000

    chore: Update .gitignore for local artifacts

--- frontend/src/app/recent-searches/recent-searches.component.spec.ts
+++ frontend/src/app/recent-searches/recent-searches.component.spec.ts
@@ -1,4 +1,5 @@
 import { type ComponentFixture, TestBed } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
 
 import { RecentSearchesComponent } from './recent-searches.component'
 
@@ -9,7 +10,7 @@ describe('RecentSearchesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RecentSearchesComponent]
+      imports: [RecentSearchesComponent, RouterTestingModule]
     })
       .compileComponents()
 
